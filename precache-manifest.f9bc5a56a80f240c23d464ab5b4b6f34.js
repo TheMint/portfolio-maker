cb999c318ebb946d547e49c6e04ffc2a4d120e1f
Yotam Ran <yotam.ran@gmail.com>
2020-04-27 21:09:58 +0300

    Updates

---
+++ precache-manifest.f9bc5a56a80f240c23d464ab5b4b6f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b5d61b58325f55beaf8ecef2f2110f",
+    "url": "/portfolio-maker/index.html"
+  },
+  {
+    "revision": "16193aa89714de202ff9",
+    "url": "/portfolio-maker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a90f8bb0d647b6bef516",
+    "url": "/portfolio-maker/static/js/2.45419c78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-maker/static/js/2.45419c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16193aa89714de202ff9",
+    "url": "/portfolio-maker/static/js/main.61f73249.chunk.js"
+  },
+  {
+    "revision": "d1e4f7190e657eee349f",
+    "url": "/portfolio-maker/static/js/runtime-main.d5d6144d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-maker/static/media/logo.5d5d9eef.svg"
+  }
+]);
