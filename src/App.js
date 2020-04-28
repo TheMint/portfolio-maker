4c774bf7ccfdda7c15708ae250cb2b8a9a970f43
Yotam Ran <yotam.ran@gmail.com>
2020-04-28 14:19:34 +0300

    Added Header stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './comps/Header'
+import TextBox from './comps/TextBox'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src="https://images-na.ssl-images-amazon.com/images/I/81-yKbVND-L.png" className="App-logo" alt="logo" />
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Header/>
       </header>
     </div>
   );
--- src/comps/Header.js
+++ src/comps/Header.js
@@ -11,7 +11,7 @@ class Header extends Component {
     {
         return(
             <div>
-                
+                <h1>Header</h1>
             </div>
         )
     }
