aacc0ae74a22aebb2bd96a200ccfb340dd8eb7e0
Yotam Ran <yotam.ran@gmail.com>
2020-04-28 14:14:29 +0300

    Added template to header.

---
+++ src/comps/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    constructor()
+    {
+        super()
+
+    }
+
+    render()
+    {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default Header
